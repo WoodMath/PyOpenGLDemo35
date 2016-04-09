ad76953d86ce5debbd27da1b9c1a27da1205fbe1
WoodMath <jeffwood314@gmail.com>
2016-04-09 14:51:09 -0500

    Added 'lesson6.py' and 'lesson6-multi.py' from 'NeHe' folder. 'lesson6-multi.py' still has issues. Updated 'makechanges.txt'

--- PyOpenGL-Demo/NeHe/lesson6.py
+++ PyOpenGL-Demo/NeHe/lesson6.py
@@ -1,12 +1,12 @@
-#!
+#!/usr/bin/env python3.4
 
 # This is statement is required by the build system to query build info
 if __name__ == '__build__':
 	raise Exception
 
 import string
-__version__ = string.split('$Revision: 1.1.1.1 $')[1]
-__date__ = string.join(string.split('$Date: 2007/02/15 19:25:21 $')[1:3], ' ')
+__version__ = '$Revision: 1.1.1.1 $'.split()[1]
+__date__ = '$Date: 2007/02/15 19:25:21 $'.split()[1:3]
 __author__ = 'Tarn Weisner Burton <twburton@users.sourceforge.net>'
 
 #
@@ -47,11 +47,11 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import sys
-from Image import *
+from PIL import Image
 
 # Some api in the chain is translating the keystrokes to this octal string
 # so instead of saying: ESCAPE = 27, we use the following.
-ESCAPE = '\033'
+ESCAPE = b'\x1b'
 
 # Number of the glut window.
 window = 0
@@ -63,11 +63,11 @@ texture = 0
 
 def LoadTextures():
     #global texture
-    image = open("NeHe.bmp")
+    image = Image.open("NeHe.bmp")
 	
     ix = image.size[0]
     iy = image.size[1]
-    image = image.tostring("raw", "RGBX", 0, -1)
+    image = image.tobytes("raw", "RGBX", 0, -1)
 	
     # Create Texture	
     glBindTexture(GL_TEXTURE_2D, glGenTextures(1))   # 2d texture (x and y size)
@@ -224,6 +224,6 @@ def main():
 	glutMainLoop()
 
 # Print message to console, and kick off the main to get it rolling.
-print "Hit ESC key to quit."
+print("Hit ESC key to quit.")
 main()
     	
